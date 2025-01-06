ff6d9bb8d1c6599977b4dd295c301bc321d85c0c
afaragi <afaragi@zobristinc.com>
2025-01-06 04:41:20 +0100

    roomsListing + booking + management

--- src/api/bookingApi.ts
+++ src/api/bookingApi.ts
@@ -1,7 +1,7 @@
 // services/api/bookingManagementApi.ts
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://mabookingbackend.onrender.com/api';
 
 export interface Booking {
     _id: string;
--- src/api/roomApi.ts
+++ src/api/roomApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://mabookingbackend.onrender.com/api';
 
 export interface RoomFormData {
   name: string;
